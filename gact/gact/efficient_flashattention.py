e9b8f0455a63ca056bfa6f74abd693641051edb0
Ther <1329438302@qq.com>
2024-03-16 16:32:20 +0800

    chore: modify softmax compress reaction

--- gact/gact/efficient_flashattention.py
+++ gact/gact/efficient_flashattention.py
@@ -4,25 +4,20 @@ import torch.nn.functional as F
 from gact.jpeg_processor import JPEGProcessor
 from gact.dct_processor import DCTProcessor
 
-from gact.memory_efficient_function import per_block_quantization, per_block_dequantization, dct_compression, jpeg_compression, naive_adjustment, mask_process
+from gact.memory_efficient_function import per_block_quantization, per_block_dequantization, dct_compression, jpeg_compression, naive_adjustment
+from transformers.models.mistral.modeling_mistral import MistralFlashAttention2
 
-
-class SoftmaxController():
-    def __init__(self, compress_type: str = "JPEG", quantization_shape: int = 64, jpeg_processor: JPEGProcessor = None, dct_processor: DCTProcessor = None, pruning: bool = False, pruning_val = -100.):
+class FlashAttentionController():
+    def __init__(self, compress_type: str = "JPEG", quantization_shape: int = 64, jpeg_processor: JPEGProcessor = None, dct_processor: DCTProcessor = None):
         self.compress_type = compress_type
         self.jpeg_processor = jpeg_processor
         self.dct_processor = dct_processor
         self.quantization_shape = quantization_shape
-        self.pruning = pruning
-        self.pruning_val = -100.
     
     def pack(self, x):
         self.original_shape = x.shape
 
         if self.compress_type != 'NONE':
-            # merge the first & second dimension
-            x = x.view(self.original_shape[0] * self.original_shape[1], *self.original_shape[2:])
-
             input_shape = x.shape
             self.input_shape = input_shape
 
@@ -30,8 +25,6 @@ class SoftmaxController():
             x, quant_state = per_block_quantization(x, input_shape, self.quantization_shape)
             self.quant_state = quant_state
 
-            if self.pruning:
-                x[x < self.pruning_val] = -128
             # compression
             if self.compress_type == 'JPEG':
                 x = jpeg_compression(x, input_shape, self.jpeg_processor, self.quantization_shape)
@@ -49,17 +42,16 @@ class SoftmaxController():
         return x
 
 
-class EfficientMemorySoftmax(nn.Module):
-    def __init__(self, dim, compress_type: str = "JPEG", compress_quality: int = 50, quantization_shape: int = 64, pruning: bool = False, pruning_val = -100.):
-        super(EfficientMemorySoftmax, self).__init__()
-        self.dim = dim
+class EfficientMemoryFlashAttention(MistralFlashAttention2):
+    def __init__(self, config, layer_idx, compress_type: str = "JPEG", compress_quality: int = 50, quantization_shape: int = 64):
+        super(EfficientMemoryFlashAttention, self).__init__(config, layer_idx)
         self.compress_type = compress_type
         self.compress_quality = compress_quality
         self.jpeg_processor = JPEGProcessor(quality=compress_quality)
         self.dct_processor = DCTProcessor(quality=compress_quality, interpolation=quantization_shape / 64)
         self.quantization_shape = quantization_shape
-        self.controller = SoftmaxController(compress_type, quantization_shape, self.jpeg_processor, self.dct_processor, pruning, pruning_val)
+        self.controller = FlashAttentionController(compress_type, quantization_shape, self.jpeg_processor, self.dct_processor)
 
-    def forward(self, input):
+    def forward(self, *args, **kwargs):
         with torch.autograd.graph.saved_tensors_hooks(self.controller.pack, self.controller.unpack):
-            return F.softmax(input, self.dim)
+            return super().forward(*args, **kwargs)
